642226860a5d6588724c71187d713ae5913bc75e
hobbitKrd <za.css@yandex.ru>
2022-07-09 19:20:33 +0300

    update HttpPostRequest class

--- TextClassification/ProcessingClass/HttpPostRequest.cs
+++ TextClassification/ProcessingClass/HttpPostRequest.cs
@@ -16,15 +16,13 @@ namespace TextClassification.ProcessingClass
         public string TextLanguage { get; set; }
         public string TextVector { get; set; }
         public string UserName { get; set; }
-        public float[] GetVectorFromWebAPI(string textLanguage, string[] Text)
+        public float[]? GetVectorFromWebAPI(string textLanguage, string[] Text)
         {
             try
-            {
-                float[] WebAPIrequest;
+            {    
                 using (var client = new HttpClient())
                 {
-                    var adressToMainServer = File.ReadAllText("ConnectString.txt");
-                    var adressToApi = new Uri(adressToMainServer + "/api/Vectorize");
+                    var adressToApi = new Uri("https://localhost:5001/api/Vectorize");
                     var SendParam = new HttpPostRequest()
                     {
                         TextLanguage = textLanguage,
@@ -33,18 +31,16 @@ namespace TextClassification.ProcessingClass
                     };
                     var NewJsonObject = JsonConvert.SerializeObject(SendParam);
                     var payLoad = new StringContent(NewJsonObject, Encoding.UTF8, "application/json");
-                    WebAPIrequest = client.PostAsync(adressToApi, payLoad).Result.Content
-                        .ReadAsStringAsync().Result
-                        .Replace("[", "")
-                        .Replace("]", "")
-                        .Replace(",", "/")
-                        .Replace(".", ",")
-                        .Split('/')
-                        .ToList()
+                    var result = client.PostAsync(adressToApi, payLoad).Result.Content
+                        .ReadAsStringAsync().Result;
+
+                    var WebAPIrequest = ClearJsonAnser(result)
                         .ConvertAll(x => float.Parse(x))
                         .ToArray();
+
+                    return WebAPIrequest;
                 }
-                return WebAPIrequest;
+               
             }
             catch (Exception ex)
             {
@@ -56,19 +52,30 @@ namespace TextClassification.ProcessingClass
         }
         public bool isServerRunning()
         {
-            try {
+            try
+            {
                 var result = string.Empty;
                 using (var client = new HttpClient())
                 {
                     var adressToMainServer = File.ReadAllText("ConnectString.txt");
-                    var adressToApi = new Uri(adressToMainServer+"/api/Vectorize");
+                    var adressToApi = new Uri(adressToMainServer + "/api/Vectorize");
                     var resultGetMethod = client.GetAsync(adressToApi).Result;
                     result = resultGetMethod.Content.ReadAsStringAsync().Result;
                 }
                 return true;
             }
             catch { return false; }
-            
+
+        }
+        private List<string> ClearJsonAnser(string rquestResult)
+        {
+            return rquestResult.Replace("[", "")
+                        .Replace("]", "")
+                        .Replace(",", "/")
+                        .Replace(".", ",")
+                        .Split('/')
+                        .ToList();
+                        
         }
     }
 }
